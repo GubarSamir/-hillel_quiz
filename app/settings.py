8d640ebaf7c62912cbf9260917e425b04d6331b9
Maxkindev <gubarsamir@gmail.com>
2021-08-08 15:00:05 +0300

    HW22

--- app/settings.py
+++ app/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'accounts.apps.AccountsConfig',
     'crispy_forms',
+    'quiz.apps.QuizConfig',
 ]
 
 MIDDLEWARE = [
@@ -120,7 +121,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / 'static'
+# STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static/'
+]
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
